67ff9992ac27014b1b723b9e10606f1cef9f4305
Nikcrysis <nikcrysis@mail.ru>
2015-09-23 11:29:13 +0200

    changes Nikita

---
+++ frontend/models/Category.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use app\models\Logotypes;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 100]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+
+    public function getLogos()
+    {
+        return $this->hasMany(Logotypes::className(), (['category' => 'id']));
+    }
+
+
+}
